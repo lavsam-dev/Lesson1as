ab51bdf37f8972aaae4e66e8984c751674cb7b9e
lavsam-dev <lavsam@gmail.com>
2021-11-30 22:23:20 +0400

    MVP+Retrofit RepositoryImpl.kt застрял

--- app/src/main/java/lavsam/gb/profias/lesson1as/data/RepositoryImpl.kt
+++ app/src/main/java/lavsam/gb/profias/lesson1as/data/RepositoryImpl.kt
@@ -1,7 +1,25 @@
 package lavsam.gb.profias.lesson1as.data
 
+import lavsam.gb.profias.lesson1as.domain.Retrofit.Common
 import lavsam.gb.profias.lesson1as.domain.repositories.BaseRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-//class RepositoryImpl : BaseRepository {
-////    override fun providedata(): String = "Some string"
-//}
+class RepositoryImpl : BaseRepository {
+    lateinit var mService: BaseRepository
+    override fun providedata(): {
+        mService = Common.retrofitService
+        mService.providedata().enqueue(object : Callback<Elements> {
+            override fun onResponse(call: Call<Elements>, response: Response<Elements>) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(call: Call<Elements>, t: Throwable) {
+
+            }
+        }
+
+        )
+    }
+}
